97ba1af1149b9bc3b1d5774e1cda9bfe8c75556e
George Castillo <georgecastillo417@gmail.com>
2024-06-01 17:25:29 -0500

    First commit

---
+++ src/main/java/com/devsu/BusinessClientManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessClientManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessClientManagementServiceApplication.class, args);
+	}
+
+}
